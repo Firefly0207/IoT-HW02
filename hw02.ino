eda695a528021a7ab80a1f113705648a84d4a581
Firefly0207 <65028806+Firefly0207@users.noreply.github.com>
2025-05-09 16:54:25 +0900

    Update hw02.ino

--- hw02.ino
+++ hw02.ino
@@ -2,16 +2,17 @@ const int buttonPin = 18;
 const int ledPin = 23;    
 
 void setup() {
+  
+  Serial.begin(115200);
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  int buttonState = digitalRead(buttonPin);
-
-  if (buttonState == HIGH) {
-    digitalWrite(ledPin, HIGH); 
-  } else {
-    digitalWrite(ledPin, LOW);   
-  }
+  int state = digitalRead(buttonPin);
+  Serial.println(state);
+  digitalWrite(ledPin, state);  
+  delay(200);
 }
